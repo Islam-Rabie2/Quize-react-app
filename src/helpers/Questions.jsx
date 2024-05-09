d91207b51a87583f5619595b862013382f950826
Islam-Rabie2 <islam.rabie25@gmail.com>
2024-05-09 13:07:21 -0700

    first commit

---
+++ src/helpers/Questions.jsx
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+      prompt: "5 + 5",
+      optionA: "20",
+      optionB: "15",
+      optionC: "25",
+      optionD: "10",
+      asnwer: "optionD",
+    },
+    {
+      prompt: "22 + 11",
+      optionA: "11",
+      optionB: "32",
+      optionC: "33",
+      optionD: "30",
+      asnwer: "optionC",
+    },
+    {
+      prompt: "55 - 25",
+      optionA: "20",
+      optionB: "25",
+      optionC: "35",
+      optionD: "30",
+      asnwer: "optionD",
+    },
+  ];
